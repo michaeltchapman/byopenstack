2c56b05fd56fff31f7c0a732ae25e1e30c1aa14a
Michael Chapman <woppin@gmail.com>
2014-09-11 03:50:35 +1000

    Fix keypair chmod

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ wget --quiet http://download.cirros-cloud.net/0.3.2/cirros-0.3.2-x86_64-disk.img
 glance add name='cirros_image' is_public=true container_format=bare disk_format=qcow2 < cirros-0.3.2-x86_64-disk.img
 
 nova keypair-add test > test.private
-chmod 0600 test
+chmod 0600 test.private
 
 neutron net-create net1
 neutron subnet-create net1 10.0.0.0/24
